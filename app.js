4a9a38617ed30dd57ac9a3bea51120ed289003a2
Garrett Chitwood <gman@acvwa.com>
2026-01-20 13:22:07 -0800

    Started Exercise 1, and prepared sections for remaining exercises.

--- app.js
+++ app.js
@@ -3,5 +3,25 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    for (let i = 0; i<= count; i++) {
+        if (i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+
+printOdds(count);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");

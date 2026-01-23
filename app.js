d1f5a5f699d999a8c7417e7b1df555b6364be040
Garrett Chitwood <gman@acvwa.com>
2026-01-22 18:51:33 -0800

    Completed Exercise 2, had to remove a else (age < 16) due to being incorrect and problematic.

--- app.js
+++ app.js
@@ -4,18 +4,40 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 
 function printOdds(count) {
-    for (let i = 0; i<= count; i++) {
-        if (i % 2 != 0) {
-            console.log(i);
+    if (count < 0) {
+        for (let i = 0; i >= count; i--) {
+            if (i % 2 != 0) {
+                console.log(i);
+            }
+        }
+    } else {
+        for (let i = 0; i<= count; i++) {
+            if (i % 2 != 0) {
+                console.log(i);
+            }
         }
     }
 }
-
-printOdds(count);
+printOdds(-100);
+console.log("--------------------")
+printOdds(100)
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
-
+function checkAge(userName, age) {
+    let aboveSixteen = `Congrats ${userName}, you can drive!`
+    let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16`
+    {
+        if (age > 16) {
+            console.log(aboveSixteen)
+        } else
+            console.log(belowSixteen)
+        }
+    }
+    checkAge("Garrett", 25);
+    checkAge("Rick", 5);
+    checkAge("John", 13);
+    checkAge("Ira", 35);
 
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");

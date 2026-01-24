329271c49dc5ca561b7088436c17324fc3e625b3
Garrett Chitwood <gman@acvwa.com>
2026-01-23 17:36:15 -0800

    Redesigned the responses for Exercise 3 since before it looked like it was counting up, instead of referencing specific locations on a graph. Also formatting.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ function printOdds(count) {
 printOdds(-100);
 console.log("--------------------")
 printOdds(100)
+console.log(" ")
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
@@ -38,18 +39,19 @@ function checkAge(userName, age) {
     checkAge("Rick", 5);
     checkAge("John", 13);
     checkAge("Ira", 35);
+    console.log(" ")
 
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 function quadrantLocator(x, y) {
     if (x > 0 && y > 0) {
-        return 1;
+        return "Quadrant 1";
     } else if (x < 0 && y > 0) {
-        return 2;
+        return "Quadrant 2";
     } else if (x < 0 && y < 0) {
-        return 3;
+        return "Quadrant 3";
     } else if (x > 0 && y < 0) {
-        return 4;
+        return "Quadrant 4";
     } else if (x == 0 && y != 0) {
         return "X-Axis";
     } else if (x != 0 && y == 0) {
@@ -58,6 +60,13 @@ function quadrantLocator(x, y) {
         return "Origin";
     }
 } console.log(quadrantLocator(1, 1));
+console.log(quadrantLocator(-2, 2));
+console.log(quadrantLocator(-3, -3));
+console.log(quadrantLocator(4, -4));
+console.log(quadrantLocator(5, 0));
+console.log(quadrantLocator(0, 6));
+console.log(quadrantLocator(0, 0));
+console.log(" ")
 
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
